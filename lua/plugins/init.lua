c41bf2b9da7f2800336a98f0892271d2898eda65
Philip Dobranowski <dobranowskip1@gmail.com>
2025-01-21 22:17:40 -0500

    Initial config upload

---
+++ lua/plugins/init.lua
@@ -0,0 +1,27 @@
+return {
+  -- conform.nvim
+  {
+    "stevearc/conform.nvim",
+    -- event = 'BufWritePre', -- uncomment for format on save
+    opts = require "configs.conform",
+  },
+
+  -- nvim-lspconfig
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "configs.lspconfig"
+    end,
+  },
+
+  -- nvim-treesitter
+  {
+  	"nvim-treesitter/nvim-treesitter",
+  	opts = {
+  		ensure_installed = {
+  			"vim", "lua", "vimdoc",
+        "html", "css", "java"
+  		},
+  	},
+  },
+}
